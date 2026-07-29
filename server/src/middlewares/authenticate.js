49dea19df1ce714e4fc233b9713828cbb3b4b73b
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-07-29 21:47:12 +0530

    Refactor middleware code for improved readability and consistency

--- server/src/middlewares/authenticate.js
+++ server/src/middlewares/authenticate.js
@@ -1,20 +1,19 @@
+const AppError = require("../utils/appError");
+const { verifyAccessToken } = require("../utils/jwt.utils");
+const authenticate = async (req, res, next) => {
+  const header = req.headers.authorization;
+  if (!header || !header.startsWith("Bearer ")) {
+    throw new AppError("token not provided", 401);
+  }
+  const token = header.split(" ")[1];
+  let decoded;
+  try {
+    decoded = await verifyAccessToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    throw new AppError("Invalid token", 401);
+  }
+};
 
- const AppError = require("../utils/appError");
-const {verifyAccessToken} = require("../utils/jwt.utils")
-const authenticate = async(req,res,next) =>{
-     const header = req.headers.authorization;
-     if(!header || !header.startsWith("Bearer ")){
-        throw new AppError("token not provided",401);
-     }
-     const token = header.split(" ")[1];
-    let decoded;
-     try{
-        decoded = await verifyAccessToken(token);
-        req.user = decoded;
-        next();
-     }catch(err){
-        throw new AppError("Invalid token",401);
-     }
-}
-
-module.exports = authenticate
+module.exports = authenticate;
--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,10 +1,10 @@
-async function errorHandler(err,req,res,next){
-    const statusCode = err.statusCode || 500
-    const message = err.message || "Internal server error"
-    return res.status(statusCode).json({
-        success:false,
-        message
-    })
+async function errorHandler(err, req, res, next) {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
 }
 
-module.exports = {errorHandler}
+module.exports = { errorHandler };
